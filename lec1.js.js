496a92d05ce529a3ec4067830115278eab3b8f65
sam.web_2 <167510798+Samarthweb2@users.noreply.github.com>
2024-08-31 11:10:21 +0530

    Add files via upload

---
+++ lec1.js.js
@@ -0,0 +1,7 @@
+console.log('helloworld');
+
+const num1 = 5;
+const num2 = 10;
+const num3 = 15;
+const sum = num1 + num2 + num3;
+console.log("The sum of the three numbers is: " + sum);
---
+++ lec2.js
@@ -0,0 +1,6 @@
+console.log("sam learning js today")
+let a=67 // a contains 67
+console.log(a)
+a="life sucks"
+console.log(a)
+//this lec is about using variable and we will further study about variable in lec3
